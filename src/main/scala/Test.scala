88a36a7676524472b1b814e63cc13a57f8877028
BaiJian666 <bj358602183@163.com>
2021-03-22 17:46:37 +0800

    "第一次提交"

---
+++ src/main/scala/Test.scala
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @ClassName Test
+ * @Author baijian
+ * @date 2021/03/22 17:44
+ */
+object Test {
+  def main(args: Array[String]): Unit = {
+    println("hello git")
+  }
+}
